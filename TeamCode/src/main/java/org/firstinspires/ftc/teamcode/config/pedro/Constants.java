0c007eabca1f657ea8dd60b5dcea1d9a0ae66da9
ciupaa <ciupaandrei123@icloud.com>
2026-01-26 20:11:09 +0200

    Autonomie

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/pedro/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/pedro/Constants.java
@@ -43,10 +43,10 @@ public class Constants {
             .leftRearMotorName("spate_stanga")
             .leftFrontMotorName("fata_stanga")
 
-            .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE);
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
             .forwardPodY(1.18110236)
