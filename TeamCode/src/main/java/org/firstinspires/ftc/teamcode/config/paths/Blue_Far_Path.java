ba108f199cea95197a4041c9cf0d0d59fa451b13
ciupaa <ciupaandrei123@icloud.com>
2026-01-30 00:01:40 +0200

    Autonomie

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/paths/Blue_Far_Path.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.config.paths;
+
+public class Blue_Far_Path {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/paths/Red_Far_Path.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.config.paths;
+
+public class Red_Far_Path {
+}
