93e62911c49d85c0a6867cea1527659d25905e4f
Alexandru <calexandruflorin19@gmail.com>
2026-01-29 11:00:00 +0200

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/pedro/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/pedro/Constants.java
@@ -17,23 +17,23 @@ public class Constants {
 
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(9)
-            // .forwardZeroPowerAcceleration(-38.936)
-            // .lateralZeroPowerAcceleration(-65.965)
+            .forwardZeroPowerAcceleration(-38.44246)
+            .lateralZeroPowerAcceleration(-54.74851)
 
             // Primary PIDs
-            // .headingPIDFCoefficients(new PIDFCoefficients(1.0, 0, 0.01, 0))
-            // .translationalPIDFCoefficients(new PIDFCoefficients(0.3, 0, 0.0, 0.018))
-            // .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.02, 0, 0.001, 0.6, 0))
+            .headingPIDFCoefficients(new PIDFCoefficients(1.0, 0, 0.01, 0))
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.3, 0, 0.0, 0.018))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.02, 0, 0.001, 0.6, 0))
 
             // Secondary PIDs
-            // .useSecondaryHeadingPIDF(true)
-            //.secondaryHeadingPIDFCoefficients(new PIDFCoefficients(3.0, 0, 0.04, 0))
+            .useSecondaryHeadingPIDF(true)
+            .secondaryHeadingPIDFCoefficients(new PIDFCoefficients(3.0, 0, 0.04, 0))
 
-            // .useSecondaryTranslationalPIDF(true)
-            // .secondaryTranslationalPIDFCoefficients(new PIDFCoefficients(0.6, 0, 0.04, 0.02))
+            .useSecondaryTranslationalPIDF(true)
+            .secondaryTranslationalPIDFCoefficients(new PIDFCoefficients(0.6, 0, 0.04, 0.02))
 
-            // .useSecondaryDrivePIDF(true)
-            // .secondaryDrivePIDFCoefficients(new FilteredPIDFCoefficients(0.1, 0, 0.01, 0.6, 0))
+            .useSecondaryDrivePIDF(true)
+            .secondaryDrivePIDFCoefficients(new FilteredPIDFCoefficients(0.1, 0, 0.01, 0.6, 0))
             ;
 
     public static MecanumConstants driveConstants = new MecanumConstants()
