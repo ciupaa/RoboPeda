9efe1b3f69ab48c91a1d7f842a64ac4e88901df9
ciupaa <ciupaandrei123@icloud.com>
2026-01-29 18:22:06 +0200

    Autonomie

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/pedro/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/pedro/Constants.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.config.pedro;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.pedropathing.control.FilteredPIDFCoefficients;
 import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
@@ -12,7 +13,7 @@ import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-
+@Configurable
 public class Constants {
 
     public static FollowerConstants followerConstants = new FollowerConstants()
