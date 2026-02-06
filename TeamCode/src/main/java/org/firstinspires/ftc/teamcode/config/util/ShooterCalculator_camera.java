819172b53178ef278b98c4e1520701fddc217248
Alexandru <calexandruflorin19@gmail.com>
2026-02-06 13:47:31 +0200

    fesf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/util/ShooterCalculator_camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/util/ShooterCalculator_camera.java
@@ -5,15 +5,18 @@ import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.util.Range;
 
 /**
- * ShooterCalculator_camera - REGRESSION EQUATION
+ * ShooterCalculator_camera - QUARTIC REGRESSION EQUATIONS
  *
- * Uses polynomial regression calculated from Desmos to determine
+ * Uses quartic (4th degree) polynomial regression calculated from Desmos to determine
  * exact angle and velocity for any given distance.
  *
- * NEW EQUATIONS (UPDATED):
+ * NEW EQUATIONS (QUARTIC - UPDATED):
  *
- * Velocity: y = -0.0000285939x³ + 0.0243239x² - 4.86525x + 1483.56669
- * Angle:    y = -(5.72338×10⁻⁹)x³ + 0.00000542892x² - 0.00175533x + 0.846639
+ * Velocity: y = 0.0000029312x⁴ - 0.00271878x³ + 0.911705x² - 127.19172x + 7347.21042
+ * Angle:    y = -(7.91756×10⁻¹⁰)x⁴ + (7.91916×10⁻⁷)x³ - 0.00027955x² + 0.0405522x - 1.19504
+ *
+ * These equations should provide better accuracy across the full range of distances
+ * compared to the previous cubic equations.
  */
 @Config
 public class ShooterCalculator_camera {
@@ -28,38 +31,40 @@ public class ShooterCalculator_camera {
     }
 
     /**
-     * Calculates Servo Angle using Cubic Regression
+     * Calculates Servo Angle using Quartic (4th degree) Regression
      *
      * NEW EQUATION:
-     * y = -(5.72338×10⁻⁹)x³ + 0.00000542892x² - 0.00175533x + 0.846639
+     * y = -(7.91756×10⁻¹⁰)x⁴ + (7.91916×10⁻⁷)x³ - 0.00027955x² + 0.0405522x - 1.19504
      */
     public static double calculateAngle(double distanceCm) {
         double x = distanceCm;
 
         double calculatedAngle =
-                -(5.72338e-9) * Math.pow(x, 3)
-                        + 0.00000542892 * Math.pow(x, 2)
-                        - 0.00175533 * x
-                        + 0.846639;
+                -(7.91756e-10) * Math.pow(x, 4)
+                        + (7.91916e-7) * Math.pow(x, 3)
+                        - 0.00027955 * Math.pow(x, 2)
+                        + 0.0405522 * x
+                        - 1.19504;
 
         // Safety clamp: 0.0 to 1.0 (Servo Position)
         return Range.clip(calculatedAngle, 0.0, 1.0);
     }
 
     /**
-     * Calculates Motor Velocity (RPM/Ticks) using Cubic Regression
+     * Calculates Motor Velocity (RPM/Ticks) using Quartic (4th degree) Regression
      *
      * NEW EQUATION:
-     * y = -0.0000285939x³ + 0.0243239x² - 4.86525x + 1483.56669
+     * y = 0.0000029312x⁴ - 0.00271878x³ + 0.911705x² - 127.19172x + 7347.21042
      */
     public static double calculateVelocity(double distanceCm) {
         double x = distanceCm;
 
         double calculatedVel =
-                -0.0000285939 * Math.pow(x, 3)
-                        + 0.0243239 * Math.pow(x, 2)
-                        - 4.86525 * x
-                        + 1483.56669;
+                0.0000029312 * Math.pow(x, 4)
+                        - 0.00271878 * Math.pow(x, 3)
+                        + 0.911705 * Math.pow(x, 2)
+                        - 127.19172 * x
+                        + 7347.21042;
 
         // Safety clamp: 0 to 2500
         return Range.clip(calculatedVel, 0.0, 2500.0);
